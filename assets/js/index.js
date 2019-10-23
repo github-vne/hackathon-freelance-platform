6f14dc5b3c0033a2f2841ac18bc318537cdabd98
vasenking <vasenking@mail.ru>
2019-10-23 17:53:04 +0500

    root

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,46 +1,46 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  "use strict";
+  setTimeout(() => main(), 1000);
+  function main() {
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+    const SpeechGrammarList =
+      window.SpeechGrammarList || window.webkitSpeechGrammarList;
+    const SpeechRecognitionEvent =
+      window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
+    const synth = window.speechSynthesis;
+    const field = document.querySelector(".speech-recognition__words");
+    const microphoneIcon = document.querySelector(".speech-recognition__icon");
+    let txtToSay = new SpeechSynthesisUtterance("Ты чё сука.");
+    synth.speak(txtToSay);
+  }
 
-    'use strict';
-    setTimeout(()=>main(), 1000);
-    function main() {
-        const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-        const SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList;
-        const SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
-        const synth = window.speechSynthesis;
-        const field = document.querySelector('.speech-recognition__words');
-        const microphoneIcon = document.querySelector('.speech-recognition__icon');
-        let txtToSay = new SpeechSynthesisUtterance('Привет.');
-        synth.speak(txtToSay);
+  // const recognition = new SpeechRecognition();
+  // recognition.continuous = true;
+  // recognition.lang = 'ru-RU';
+  // recognition.interimResults = true;
+  // recognition.maxAlternatives = 1;
+
+  // recognition.start();
+  // field.textContent = 'Ready to receive command.';
+
+  // recognition.onresult = (event) => {
+  //     const res = (event.results[0][0].transcript);
+  //     for (let el of event.results){
+  //         console.info(el[0].transcript);
+  //         field.textContent = `Результат: ${el[0].transcript}` ;
+  //         if (el[0].transcript.toLowerCase().indexOf('войти')+1){
+  //             window.open('sign_up.html','_self');
+  //         }else if (el[0].transcript.toLowerCase().indexOf('яндекс')+1){
+  //             window.open('https://yandex.com','_self');
+  //         }
+  //     }
+  // };
+
+  $(document).keydown(function(e) {
+    if (e.keyCode === 32) {
+      e.preventDefault();
+      window.location.replace("sign_up.html");
     }
-
-    // const recognition = new SpeechRecognition();
-    // recognition.continuous = true;
-    // recognition.lang = 'ru-RU';
-    // recognition.interimResults = true;
-    // recognition.maxAlternatives = 1;
-
-    // recognition.start();
-    // field.textContent = 'Ready to receive command.';
-
-    // recognition.onresult = (event) => {
-    //     const res = (event.results[0][0].transcript);
-    //     for (let el of event.results){
-    //         console.info(el[0].transcript);
-    //         field.textContent = `Результат: ${el[0].transcript}` ;
-    //         if (el[0].transcript.toLowerCase().indexOf('войти')+1){
-    //             window.open('sign_up.html','_self');
-    //         }else if (el[0].transcript.toLowerCase().indexOf('яндекс')+1){
-    //             window.open('https://yandex.com','_self');
-    //         }
-    //     }
-    // };
-
-    $(document).keydown(function (e) {
-        if(e.keyCode === 32){
-            e.preventDefault();
-            window.location.replace("sign_up.html");
-        }
-    });
-
-
+  });
 });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,307 +1,372 @@
-
 // Generate random room name if needed
 if (!location.hash) {
-    location.hash = 'room';
+  location.hash = "123";
 }
 const roomHash = location.hash.substring(1);
 const possibleEmojis = [
-    '🐀', '🐁', '🐭', '🐹', '🐂', '🐃', '🐄', '🐮', '🐅', '🐆', '🐯', '🐇', '🐐', '🐑', '🐏', '🐴',
-    '🐎', '🐱', '🐈', '🐰', '🐓', '🐔', '🐤', '🐣', '🐥', '🐦', '🐧', '🐘', '🐩', '🐕', '🐷', '🐖',
-    '🐗', '🐫', '🐪', '🐶', '🐺', '🐻', '🐨', '🐼', '🐵', '🙈', '🙉', '🙊', '🐒', '🐉', '🐲', '🐊',
-    '🐍', '🐢', '🐸', '🐋', '🐳', '🐬', '🐙', '🐟', '🐠', '🐡', '🐚', '🐌', '🐛', '🐜', '🐝', '🐞',
+  "🐀",
+  "🐁",
+  "🐭",
+  "🐹",
+  "🐂",
+  "🐃",
+  "🐄",
+  "🐮",
+  "🐅",
+  "🐆",
+  "🐯",
+  "🐇",
+  "🐐",
+  "🐑",
+  "🐏",
+  "🐴",
+  "🐎",
+  "🐱",
+  "🐈",
+  "🐰",
+  "🐓",
+  "🐔",
+  "🐤",
+  "🐣",
+  "🐥",
+  "🐦",
+  "🐧",
+  "🐘",
+  "🐩",
+  "🐕",
+  "🐷",
+  "🐖",
+  "🐗",
+  "🐫",
+  "🐪",
+  "🐶",
+  "🐺",
+  "🐻",
+  "🐨",
+  "🐼",
+  "🐵",
+  "🙈",
+  "🙉",
+  "🙊",
+  "🐒",
+  "🐉",
+  "🐲",
+  "🐊",
+  "🐍",
+  "🐢",
+  "🐸",
+  "🐋",
+  "🐳",
+  "🐬",
+  "🐙",
+  "🐟",
+  "🐠",
+  "🐡",
+  "🐚",
+  "🐌",
+  "🐛",
+  "🐜",
+  "🐝",
+  "🐞"
 ];
 
 function randomEmoji() {
-    var randomIndex = Math.floor(Math.random() * possibleEmojis.length);
-    return possibleEmojis[randomIndex];
+  var randomIndex = Math.floor(Math.random() * possibleEmojis.length);
+  return possibleEmojis[randomIndex];
 }
-
 
 const emoji = randomEmoji();
 // const user = prompt('Enter your name:');
 const user = "Nikolay";
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 
 recognition.continuous = true;
-    recognition.lang = 'ru-RU';
-    recognition.interimResults = true;
-    recognition.maxAlternatives = 1;
-    recognition.start();
+recognition.lang = "ru-RU";
+recognition.interimResults = true;
+recognition.maxAlternatives = 1;
+recognition.start();
 
-const drone = new ScaleDrone('pMbakAnQeahIRBtF');
-const roomName = 'observable-' + roomHash;
+const drone = new ScaleDrone("pMbakAnQeahIRBtF");
+const roomName = "observable-" + roomHash;
 const configuration = {
-    iceServers: [{
-            url: 'stun:stun01.sipphone.com'
-        },
-        {
-            url: 'stun:stun.ekiga.net'
-        },
-        {
-            url: 'stun:stun.fwdnet.net'
-        },
-        {
-            url: 'stun:stun.ideasip.com'
-        },
-        {
-            url: 'stun:stun.iptel.org'
-        },
-        {
-            url: 'stun:stun.rixtelecom.se'
-        },
-        {
-            url: 'stun:stun.schlund.de'
-        },
-        {
-            url: 'stun:stun.l.google.com:19302'
-        },
-        {
-            url: 'stun:stun1.l.google.com:19302'
-        },
-        {
-            url: 'stun:stun2.l.google.com:19302'
-        },
-        {
-            url: 'stun:stun3.l.google.com:19302'
-        },
-        {
-            url: 'stun:stun4.l.google.com:19302'
-        },
-        {
-            url: 'stun:stunserver.org'
-        },
-        {
-            url: 'stun:stun.softjoys.com'
-        },
-        {
-            url: 'stun:stun.voiparound.com'
-        },
-        {
-            url: 'stun:stun.voipbuster.com'
-        },
-        {
-            url: 'stun:stun.voipstunt.com'
-        },
-        {
-            url: 'stun:stun.voxgratia.org'
-        },
-        {
-            url: 'stun:stun.xten.com'
-        },
-        {
-            url: 'turn:numb.viagenie.ca',
-            credential: 'muazkh',
-            username: 'webrtc@live.com'
-        },
-        {
-            url: 'turn:192.158.29.39:3478?transport=udp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
-        },
-        {
-            url: 'turn:192.158.29.39:3478?transport=tcp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
-        }
-    ]
+  iceServers: [
+    {
+      url: "stun:stun01.sipphone.com"
+    },
+    {
+      url: "stun:stun.ekiga.net"
+    },
+    {
+      url: "stun:stun.fwdnet.net"
+    },
+    {
+      url: "stun:stun.ideasip.com"
+    },
+    {
+      url: "stun:stun.iptel.org"
+    },
+    {
+      url: "stun:stun.rixtelecom.se"
+    },
+    {
+      url: "stun:stun.schlund.de"
+    },
+    {
+      url: "stun:stun.l.google.com:19302"
+    },
+    {
+      url: "stun:stun1.l.google.com:19302"
+    },
+    {
+      url: "stun:stun2.l.google.com:19302"
+    },
+    {
+      url: "stun:stun3.l.google.com:19302"
+    },
+    {
+      url: "stun:stun4.l.google.com:19302"
+    },
+    {
+      url: "stun:stunserver.org"
+    },
+    {
+      url: "stun:stun.softjoys.com"
+    },
+    {
+      url: "stun:stun.voiparound.com"
+    },
+    {
+      url: "stun:stun.voipbuster.com"
+    },
+    {
+      url: "stun:stun.voipstunt.com"
+    },
+    {
+      url: "stun:stun.voxgratia.org"
+    },
+    {
+      url: "stun:stun.xten.com"
+    },
+    {
+      url: "turn:numb.viagenie.ca",
+      credential: "muazkh",
+      username: "webrtc@live.com"
+    },
+    {
+      url: "turn:192.158.29.39:3478?transport=udp",
+      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+      username: "28224511:1379330808"
+    },
+    {
+      url: "turn:192.158.29.39:3478?transport=tcp",
+      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+      username: "28224511:1379330808"
+    }
+  ]
 };
 let room;
 let pc;
 
-
-function onSuccess() {};
+function onSuccess() {}
 
 function onError(error) {
-    console.error(error);
-};
+  console.error(error);
+}
 
-drone.on('open', error => {
+drone.on("open", error => {
+  if (error) {
+    return console.error(error);
+  }
+  room = drone.subscribe(roomName);
+  room.on("open", error => {
     if (error) {
-        return console.error(error);
+      onError(error);
     }
-    room = drone.subscribe(roomName);
-    room.on('open', error => {
-        if (error) {
-            onError(error);
-        }
-    });
-    room.on('members', members => {
-        console.log('MEMBERS', members);
-        const isOfferer = members.length === 2;
-        startWebRTC(isOfferer);
-    });
+  });
+  room.on("members", members => {
+    console.log("MEMBERS", members);
+    const isOfferer = members.length === 2;
+    startWebRTC(isOfferer);
+  });
 });
 
 function sendMessage(message) {
-    drone.publish({
-        room: roomName,
-        message
-    });
+  drone.publish({
+    room: roomName,
+    message
+  });
 }
 
 function startWebRTC(isOfferer) {
-    pc = new RTCPeerConnection(configuration);
+  pc = new RTCPeerConnection(configuration);
 
-    pc.onicecandidate = event => {
-        if (event.candidate) {
-            sendMessage({
-                'candidate': event.candidate
-            });
-        }
-    };
-
-
-    if (isOfferer) {
-        pc.onnegotiationneeded = () => {
-            pc.createOffer(localDescCreated, error => console.error(error));
-        }
-        dataChannel = pc.createDataChannel('chat');
-        setupDataChannel();
-    } else {
-        pc.ondatachannel = event => {
-            dataChannel = event.channel;
-            setupDataChannel();
-        }
+  pc.onicecandidate = event => {
+    if (event.candidate) {
+      sendMessage({
+        candidate: event.candidate
+      });
     }
+  };
 
-    pc.ontrack = event => {
-        const stream = event.streams[0];
-        if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
-            remoteVideo.srcObject = stream;
-        }
+  if (isOfferer) {
+    pc.onnegotiationneeded = () => {
+      pc.createOffer(localDescCreated, error => console.error(error));
     };
+    dataChannel = pc.createDataChannel("chat");
+    setupDataChannel();
+  } else {
+    pc.ondatachannel = event => {
+      dataChannel = event.channel;
+      setupDataChannel();
+    };
+  }
 
-    navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: true,
-    }).then(stream => {
-        localVideo.srcObject = stream;
-        stream.getTracks().forEach(track => pc.addTrack(track, stream));
+  pc.ontrack = event => {
+    const stream = event.streams[0];
+    if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
+      remoteVideo.srcObject = stream;
+    }
+  };
+
+  navigator.mediaDevices
+    .getUserMedia({
+      audio: false,
+      video: true
+    })
+    .then(stream => {
+      localVideo.srcObject = stream;
+      stream.getTracks().forEach(track => pc.addTrack(track, stream));
     }, onError);
-    startListentingToSignals();
+  startListentingToSignals();
 }
 
 function startListentingToSignals() {
-    room.on('data', (message, client) => {
-        if (client.id === drone.clientId) {
-            return;
-        }
+  room.on("data", (message, client) => {
+    if (client.id === drone.clientId) {
+      return;
+    }
 
-        if (message.sdp) {
-            pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
-                if (pc.remoteDescription.type === 'offer') {
-                    pc.createAnswer().then(localDescCreated).catch(onError);
-                }
-            }, onError);
-        } else if (message.candidate) {
-            pc.addIceCandidate(
-                new RTCIceCandidate(message.candidate), onSuccess, onError
-            );
-        }
-    });
+    if (message.sdp) {
+      pc.setRemoteDescription(
+        new RTCSessionDescription(message.sdp),
+        () => {
+          if (pc.remoteDescription.type === "offer") {
+            pc.createAnswer()
+              .then(localDescCreated)
+              .catch(onError);
+          }
+        },
+        onError
+      );
+    } else if (message.candidate) {
+      pc.addIceCandidate(
+        new RTCIceCandidate(message.candidate),
+        onSuccess,
+        onError
+      );
+    }
+  });
 }
 
 function localDescCreated(desc) {
-    pc.setLocalDescription(
-        desc,
-        () => sendMessage({
-            'sdp': pc.localDescription
-        }),
-        onError
-    );
+  pc.setLocalDescription(
+    desc,
+    () =>
+      sendMessage({
+        sdp: pc.localDescription
+      }),
+    onError
+  );
 }
 
 function setupDataChannel() {
-    checkDataChannelState();
-    dataChannel.onopen = checkDataChannelState;
-    dataChannel.onclose = checkDataChannelState;
-    dataChannel.onmessage = event => {
-        const jsonData = JSON.parse(event.data);
-        if (jsonData.type == "chat-message") {
-            insertMessageToDOM(jsonData, false);
-        } else {
-        console.info(jsonData.content,'субтитры');
-            user !== jsonData.user ? processSubtitles(jsonData) : false;
-        }
-    };
+  checkDataChannelState();
+  dataChannel.onopen = checkDataChannelState;
+  dataChannel.onclose = checkDataChannelState;
+  dataChannel.onmessage = event => {
+    const jsonData = JSON.parse(event.data);
+    if (jsonData.type == "chat-message") {
+      insertMessageToDOM(jsonData, false);
+    } else {
+      console.info(jsonData.content, "субтитры");
+      user !== jsonData.user ? processSubtitles(jsonData) : false;
+    }
+  };
 }
 
 function checkDataChannelState() {
-    console.log('WebRTC channel state is:', dataChannel.readyState);
+  console.log("WebRTC channel state is:", dataChannel.readyState);
 }
 
-recognition.onresult = (event) => {
-    const res = (event.results[0][0].transcript);
-    for (let el of event.results){
-        console.info(el[0].transcript);
-        sendSubtitles(el[0].transcript)
-        // sendSubtitles(el[0].transcript, user);
-    }
+recognition.onresult = event => {
+  const res = event.results[0][0].transcript;
+  for (let el of event.results) {
+    console.info(el[0].transcript);
+    sendSubtitles(el[0].transcript);
+    // sendSubtitles(el[0].transcript, user);
+  }
 };
 
 function processSubtitles(options) {
-    const subtitles = document.getElementById('subtitles');
-    subtitles.innerText = options.content;
-    //TO DO  
+  const subtitles = document.getElementById("subtitles");
+  subtitles.innerText = options.content;
+  //TO DO
 }
 
 function insertMessageToDOM(options, isFromMe) {
-    const template = document.querySelector('template[data-template="message"]');
-    const nameEl = template.content.querySelector('.message__name');
-    if (options.emoji || options.name) {
-        nameEl.innerText = options.emoji + ' ' + options.name;
-    }
-    template.content.querySelector('.message__bubble').innerText = options.content;
-    const clone = document.importNode(template.content, true);
-    const messageEl = clone.querySelector('.message');
-    if (isFromMe) {
-        messageEl.classList.add('message--mine');
-    } else {
-        messageEl.classList.add('message--theirs');
-    }
+  const template = document.querySelector('template[data-template="message"]');
+  const nameEl = template.content.querySelector(".message__name");
+  if (options.emoji || options.name) {
+    nameEl.innerText = options.emoji + " " + options.name;
+  }
+  template.content.querySelector(".message__bubble").innerText =
+    options.content;
+  const clone = document.importNode(template.content, true);
+  const messageEl = clone.querySelector(".message");
+  if (isFromMe) {
+    messageEl.classList.add("message--mine");
+  } else {
+    messageEl.classList.add("message--theirs");
+  }
 
-    const messagesEl = document.querySelector('.messages');
-    messagesEl.appendChild(clone);
+  const messagesEl = document.querySelector(".messages");
+  messagesEl.appendChild(clone);
 
-    // Scroll to bottom
-    messagesEl.scrollTop = messagesEl.scrollHeight - messagesEl.clientHeight;
+  // Scroll to bottom
+  messagesEl.scrollTop = messagesEl.scrollHeight - messagesEl.clientHeight;
 }
 
-
-
-
-const form = document.getElementById('form_test');
+const form = document.getElementById("form_test");
 console.log(form);
 
-form.addEventListener('submit', () => {
-    if (dataChannel.readyState === 'open') {
-        const input = document.querySelector('#text');
-        const value = input.value;
-        input.value = '';
+form.addEventListener("submit", () => {
+  if (dataChannel.readyState === "open") {
+    const input = document.querySelector("#text");
+    const value = input.value;
+    input.value = "";
 
-        const data = {
-            type: "chat-message",
-            name,
-            content: value,
-            emoji,
-        };
+    const data = {
+      type: "chat-message",
+      name,
+      content: value,
+      emoji
+    };
 
-        dataChannel.send(JSON.stringify(data));
+    dataChannel.send(JSON.stringify(data));
 
-        insertMessageToDOM(data, true);
-    }
+    insertMessageToDOM(data, true);
+  }
 });
 
 function sendSubtitles(value) {
-
-    const data = {
-        type: "subtitles",
-        user: 'test',
-        content: value,
-    };
-    $('#subtitles').html(`Наставник: ${value}`);
-    // console.info(data);
-    // dataChannel.send(JSON.stringify(data));
+  const data = {
+    type: "subtitles",
+    user: "test",
+    content: value
+  };
+  $("#subtitles").html(`Наставник: ${value}`);
+  // console.info(data);
+  // dataChannel.send(JSON.stringify(data));
 }
-
